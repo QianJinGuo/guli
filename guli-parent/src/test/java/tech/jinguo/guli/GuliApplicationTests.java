ab4551733dd72bfa331c55c2567b2d3529033c53
jinguo <felix@jinguo.tech>
2020-12-18 20:43:39 +0800

    新建springboot父工程

---
+++ guli-parent/src/test/java/tech/jinguo/guli/GuliApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.jinguo.guli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
